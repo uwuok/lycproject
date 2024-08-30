f4aaf181c1c6be59cc99bfd53ca925bcdef785c9
uwuok <natsumi7129453@gmail.com>
2024-08-30 17:02:53 +0800

    ROI change backup 8/30 5:02

--- imgprotest.py
+++ imgprotest.py
@@ -3,17 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def drawpoly(img, points):
+def c():
+    points = np.array([[0, 0], [0, 11], [10, 10], [10, 0]])
+    a = cv2.contourArea(points)
+    print("area a is:", a)
+
+
+
+def draw_poly(img, points):
     cv2.polylines(img, [np.array(points)], isClosed=True, color=(255, 255, 255), thickness=10)
     return img
 
 
 def process_edge(img, points):
     img = np.zeros_like(img)
-    mask = drawpoly(img, points)
+    mask = draw_poly(img, points)
     x, y, w, h = cv2.boundingRect(points)
-    return mask[y:y+h, x:x+w]
-
+    return mask[y:y + h, x:x + w]
 
 
 def process_roi(img, points):
@@ -21,10 +27,97 @@ def process_roi(img, points):
     cv2.fillPoly(mask, [points], 255)
     masked_img = cv2.bitwise_and(img, img, mask=mask)
     x, y, w, h = cv2.boundingRect(points)
-    return masked_img[y:y+h, x:x+w]
+    return masked_img[y:y + h, x:x + w]
 
 
-def image_processed(img):
+def image_processed(img, points):
+    # 1. 轉換成灰階
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 2. 提取 ROI 圖像以及邊緣
+    roi = process_roi(img, points)
+    edge = process_edge(img, points)
+
+    # 直方圖等化提高對比度
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    eq = clahe.apply(roi)
+
+    # 雙邊濾波
+    # bf = cv2.bilateralFilter(eq, 3, 25, 50)
+    bf = cv2.GaussianBlur(eq, (15, 15), 0)
+    # top hat
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    th = cv2.morphologyEx(bf, cv2.MORPH_TOPHAT, kernel)
+
+    # 二值化
+    # _, b = cv2.threshold(th, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    b = cv2.adaptiveThreshold(th, 127, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 30)
+    # 去除邊緣（如果需要）
+    # rm_edge = cv2.bitwise_xor(b, edge)
+
+    # 形態學腐蝕，獲得整體切屑輪廓
+    eroded = cv2.erode(b, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=1)
+
+    # 膨脹
+    dilated = cv2.dilate(eroded, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
+
+    # 計算 ROI 面積
+    # roi_area = cv2.countNonZero(process_roi(np.zeros_like(img), points))
+    # roi_area = cv2.fillPoly()
+    roi_area1 = cv2.contourArea(points)
+    # # 計算二值化後圖像中的黑與白像素數量
+    # white_pixels = cv2.countNonZero(dilated)  # 0.0
+    # black_pixels = roi_area - white_pixels
+    #
+    # # 計算黑與白像素的比例
+    # white_ratio = white_pixels / roi_area
+    # black_ratio = black_pixels / roi_area
+
+    # mask = np.zeros_like(dilated)
+    # cv2.fillPoly(mask, [points], 255)
+    # roi_area = cv2.countNonZero(mask)
+    # cv2.imshow('dilated', cv2.resize(dilated, None, fx=0.1, fy=0.1))
+    # cv2.imshow('mask', cv2.resize(mask, None, fx=0.1, fy=0.1))
+    # white_pixels = cv2.countNonZero(dilated & mask)
+    # black_pixels = roi_area - white_pixels
+    # white_ratio = white_pixels / roi_area
+    # black_ratio = black_pixels / roi_area
+
+    # 計算區域內的非零值的個數
+    mask = np.ones_like(dilated)
+    print(cv2.countNonZero(mask))  # 14727801
+
+    # 計算區域內的面積
+    height, width = mask.shape
+    ps = np.array([[0, 0], [0, width], [height, width], [height, 0]])
+    print(cv2.contourArea(ps))  # # 14727801.0
+
+
+    # print(f"ROI 面積: {roi_area} 像素")
+    # print(f"ROI1 面積: {roi_area1} 像素")
+    # print(f"白色像素： {white_pixels}")
+    # print(f"黑色像素： {black_pixels}")
+    # print(f"白色像素比例: {white_ratio:.2%}")
+    # print(f"黑色像素比例: {black_ratio:.2%}")
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # 顯示結果
+    # titles = ['Grayscale', 'ROI', 'Edge', 'Equalized', 'Top Hat',
+    #           'Binary', 'Eroded', 'Dilated']
+    # images = [img, roi, edge, eq, th, b, eroded, dilated]
+    #
+    # plt.figure(figsize=(15, 10))
+    # for i in range(len(images)):
+    #     plt.subplot(2, 5, i + 1)
+    #     plt.imshow(images[i], cmap='gray')
+    #     plt.title(titles[i])
+    #     plt.axis('off')
+    # plt.show()
+
+
+if __name__ == '__main__':
     p1 = np.array([[357, 502], [1192, 552], [1112, 607],
                    [1012, 682], [932, 747], [852, 832],
                    [782, 927], [707, 1032], [657, 1127],
@@ -40,102 +133,9 @@ def image_processed(img):
                    [3010, 5990], [2630, 4310], [2590, 3970],
                    [2610, 3770], [2650, 3610], [2670, 3530],
                    [2730, 3410]])
-    # 1.轉換成灰階
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # 2.進行雙邊模糊 (提升邊緣明顯度，由於已有邊的點，因此可以省略或改成高斯模糊等，方便後續處理)
-    # img = cv2.bilateralFilter(img, 5, 50, 100)
-
-    # 2. 提取 ROI 圖像 以及 邊緣
-    a1 = process_roi(img, p1)
-    e1 = process_edge(img, p1)
-    a2 = process_roi(img, p2)
-    e2 = process_edge(img, p2)
-    a3 = process_roi(img, p3)
-    e3 = process_edge(img, p3)
-
-    cv2.imshow('a1', cv2.resize(a1, None, fx=0.4, fy=0.4))
-    cv2.imshow('a2', cv2.resize(a2, None, fx=0.1, fy=0.1))
-    cv2.imshow('a3', cv2.resize(a3, None, fx=0.1, fy=0.1))
-
-    # 3. 直方圖等化提高對比度
-    # 4. 自適應二值化
-    # 5. 去除邊緣
-    # 6. top hat
-    # 7.
-
-def image_processed_test(img, points):
-    # 以灰階圖像讀入
-    # image = cv2.imread('C:\\Users\\natsumi\\PycharmProjects\\pythonProject\\image\\data sample\\1\\photo_20240628_105242_roi2.png', cv2.IMREAD_UNCHANGED)
-    # image = cv2.GaussianBlur(image, (3, 3), 0)
-    image = cv2.imread()
-    image = cv2.bilateralFilter(image, 5, 50, 100)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = cv2.imread('deprecation/edge_mask/new_edges_mask_1.png')
-
-    # Canny 檢測邊緣
-    # edges = cv2.Canny(image, 350, 1000)  # 邊緣為白色(1)
-
-    # 膨胀边缘，使其更厚
-    kernel_dilate_edges = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
-    dilated_edges = cv2.dilate(edges, kernel_dilate_edges, iterations=1)
-
-    # 二值化
-    binary_image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 2)
-    binary_image = cv2.bitwise_not(binary_image)
-
-    # 使用膨胀后的边缘作为掩码去除二值化图像中的外轮廓
-    mask = cv2.bitwise_not(dilated_edges)
-    binary_image_without_edges = cv2.bitwise_and(binary_image, binary_image, mask=mask)
-
-    # 形态学腐蚀，获得整体切屑轮廓
-    # ksize = 2, 2
-    kernel_erode = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-    eroded = cv2.erode(binary_image_without_edges, kernel_erode, iterations=1)
-
-    # 形态学膨胀，获得整个切屑特征
-    # ksize = 3, 3
-    kernel_dilate = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    dilated = cv2.dilate(eroded, kernel_dilate, iterations=1)
-
-    # 显示结果
-    plt.figure(figsize=(12, 8))
-
-    plt.subplot(2, 5, 1)
-    plt.title('Original Image')
-    plt.imshow(image, cmap='gray')
-
-    plt.subplot(2, 5, 2)
-    plt.title('Edges Detected')
-    plt.imshow(edges, cmap='gray')
-
-    plt.subplot(2, 5, 3)
-    plt.title('Dilated Edges')
-    plt.imshow(dilated_edges, cmap='gray')
-
-    plt.subplot(2, 5, 4)
-    plt.title('Binary Image')
-    plt.imshow(binary_image, cmap='gray')
-
-    plt.subplot(2, 5, 5)
-    plt.title('Binary Image without Edges')
-    plt.imshow(binary_image_without_edges, cmap='gray')
-
-    plt.subplot(2, 5, 6)
-    plt.title('Eroded Image')
-    plt.imshow(eroded, cmap='gray')
-
-    plt.subplot(2, 5, 7)
-    plt.title('Dilated Image (result)')
-    plt.imshow(dilated, cmap='gray')
-
-    # 保存最终结果
-    cv2.imwrite('final_image.png', dilated)
-
-    plt.tight_layout()
-    plt.show()
-
-
-if __name__ == '__main__':
     image = cv2.imread('new2.png')
-    image_processed(image)
+    # image_processed(image, p1)
+    # image_processed(image, p2)
+    image_processed(image, p3)
     # f(image)
+    # c()
