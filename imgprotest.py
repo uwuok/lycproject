a863e3491da88b2f32a94226e6e1e50d02b3f582
uwuok <natsumi7129453@gmail.com>
2024-08-30 17:32:11 +0800

    ROI change backup 8/30 17:32

--- imgprotest.py
+++ imgprotest.py
@@ -39,64 +39,49 @@ def image_processed(img, points):
     edge = process_edge(img, points)
 
     # 直方圖等化提高對比度
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(3, 3))
     eq = clahe.apply(roi)
 
     # 雙邊濾波
     # bf = cv2.bilateralFilter(eq, 3, 25, 50)
-    bf = cv2.GaussianBlur(eq, (15, 15), 0)
+    bf = cv2.GaussianBlur(eq, (5, 5), 0)
     # top hat
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     th = cv2.morphologyEx(bf, cv2.MORPH_TOPHAT, kernel)
 
     # 二值化
     # _, b = cv2.threshold(th, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    b = cv2.adaptiveThreshold(th, 127, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 30)
+    b = cv2.adaptiveThreshold(th, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 33, -1)
     # 去除邊緣（如果需要）
     # rm_edge = cv2.bitwise_xor(b, edge)
 
     # 形態學腐蝕，獲得整體切屑輪廓
-    eroded = cv2.erode(b, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=1)
+    eroded = cv2.erode(b, cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1)), iterations=2)
 
     # 膨脹
-    dilated = cv2.dilate(eroded, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
+    dilated = cv2.dilate(eroded, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=2)
 
     # 計算 ROI 面積
-    # roi_area = cv2.countNonZero(process_roi(np.zeros_like(img), points))
-    # roi_area = cv2.fillPoly()
-    roi_area1 = cv2.contourArea(points)
-    # # 計算二值化後圖像中的黑與白像素數量
-    # white_pixels = cv2.countNonZero(dilated)  # 0.0
-    # black_pixels = roi_area - white_pixels
-    #
-    # # 計算黑與白像素的比例
-    # white_ratio = white_pixels / roi_area
-    # black_ratio = black_pixels / roi_area
+    roi_area = cv2.contourArea(points)
+    white_pixels = cv2.countNonZero(b)
+    black_pixels = roi_area - white_pixels
 
-    # mask = np.zeros_like(dilated)
-    # cv2.fillPoly(mask, [points], 255)
-    # roi_area = cv2.countNonZero(mask)
-    # cv2.imshow('dilated', cv2.resize(dilated, None, fx=0.1, fy=0.1))
-    # cv2.imshow('mask', cv2.resize(mask, None, fx=0.1, fy=0.1))
-    # white_pixels = cv2.countNonZero(dilated & mask)
-    # black_pixels = roi_area - white_pixels
-    # white_ratio = white_pixels / roi_area
-    # black_ratio = black_pixels / roi_area
+    # cv2.imshow('b', cv2.resize(b, None, fx=0.1, fy=0.1))
 
     # 計算區域內的非零值的個數
-    mask = np.ones_like(dilated)
-    print(cv2.countNonZero(mask))  # 14727801
+    # mask = np.ones_like(dilated)
+    # print(cv2.countNonZero(mask))  # 14727801
+    #
+    # # 計算區域內的面積
+    # height, width = mask.shape
+    # ps = np.array([[0, 0], [0, width], [height, width], [height, 0]])
+    # print(cv2.contourArea(ps))  # # 14727801.0
 
-    # 計算區域內的面積
-    height, width = mask.shape
-    ps = np.array([[0, 0], [0, width], [height, width], [height, 0]])
-    print(cv2.contourArea(ps))  # # 14727801.0
 
-
-    # print(f"ROI 面積: {roi_area} 像素")
+    print(f"ROI 面積: {roi_area} 像素")
     # print(f"ROI1 面積: {roi_area1} 像素")
-    # print(f"白色像素： {white_pixels}")
-    # print(f"黑色像素： {black_pixels}")
+    print(f"白色像素： {white_pixels}")
+    print(f"黑色像素： {black_pixels}")
     # print(f"白色像素比例: {white_ratio:.2%}")
     # print(f"黑色像素比例: {black_ratio:.2%}")
 
@@ -104,17 +89,17 @@ def image_processed(img, points):
     cv2.destroyAllWindows()
 
     # 顯示結果
-    # titles = ['Grayscale', 'ROI', 'Edge', 'Equalized', 'Top Hat',
-    #           'Binary', 'Eroded', 'Dilated']
-    # images = [img, roi, edge, eq, th, b, eroded, dilated]
-    #
-    # plt.figure(figsize=(15, 10))
-    # for i in range(len(images)):
-    #     plt.subplot(2, 5, i + 1)
-    #     plt.imshow(images[i], cmap='gray')
-    #     plt.title(titles[i])
-    #     plt.axis('off')
-    # plt.show()
+    titles = ['Grayscale', 'ROI', 'Edge', 'Equalized', 'Top Hat',
+              'Binary', 'Eroded', 'Dilated']
+    images = [img, roi, edge, eq, th, b, eroded, dilated]
+
+    plt.figure(figsize=(15, 10))
+    for i in range(len(images)):
+        plt.subplot(2, 5, i + 1)
+        plt.imshow(images[i], cmap='gray')
+        plt.title(titles[i])
+        plt.axis('off')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -135,7 +120,7 @@ if __name__ == '__main__':
                    [2730, 3410]])
     image = cv2.imread('new2.png')
     # image_processed(image, p1)
-    # image_processed(image, p2)
-    image_processed(image, p3)
+    image_processed(image, p2)
+    # image_processed(image, p3)
     # f(image)
     # c()
